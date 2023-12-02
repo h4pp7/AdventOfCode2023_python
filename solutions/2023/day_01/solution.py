378630d3adae8aae9570b85ab710963a01e8f97a
Yann Wilhelm <code@yannwilhelm.de>
2023-12-02 04:05:09 +0100

    solutions for day 1

--- solutions/2023/day_01/solution.py
+++ solutions/2023/day_01/solution.py
@@ -2,21 +2,30 @@
 
 # puzzle prompt: https://adventofcode.com/2023/day/1
 
-from ...base import TextSolution, answer
+from ...base import StrSplitSolution, answer
 
+def calibration_value(line: str) -> int:
+    digits = [char for char in line if char.isdigit()]
+    return int(digits[0] + digits[-1])
 
-class Solution(TextSolution):
+def replace_numbers(line: str) -> str:
+    numbers = {
+            "one": "one1one", "two": "two2two", "three": "three3three",
+            "four": "four4four", "five": "five5five", "six":"six6six",
+            "seven":"seven7seven", "eight":"eight8eight", "nine":"nine9nine"}
+    for num, digit in numbers.items():
+        line = line.replace(num, digit)
+    return line
+
+class Solution(StrSplitSolution):
     _year = 2023
     _day = 1
 
-    # @answer(1234)
+    @answer(55971)
     def part_1(self) -> int:
+        return sum([calibration_value(line) for line in self.input])
         pass
 
-    # @answer(1234)
+    @answer(54719)
     def part_2(self) -> int:
-        pass
-
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
+        return sum([calibration_value(replace_numbers(line)) for line in self.input])
