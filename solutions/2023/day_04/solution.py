0a94e7ba225db1cde3aa5673be2b05e3fad59097
Yann Wilhelm <yannwilhelm@yahoo.de>
2023-12-04 20:54:46 +0100

    solution for part 2 of day 4

--- solutions/2023/day_04/solution.py
+++ solutions/2023/day_04/solution.py
@@ -6,22 +6,26 @@ from collections import Counter
 
 from ...base import StrSplitSolution, answer
 
+def get_winners(input):
+    cards = [[n for n in card if n.isnumeric()]
+                for card in [line.split(" ") for line in input]]
+    return [len([k for k,v in Counter(card).items() if v>1])
+                    for card in cards]
+
 class Solution(StrSplitSolution):
     _year = 2023
     _day = 4
 
     @answer(22897)
     def part_1(self) -> int:
-        cards = [[n for n in card if n.isnumeric()]
-                    for card in [line.split(" ") for line in self.input]]
-        winning_cards = [len([k for k,v in Counter(card).items() if v>1])
-                            for card in cards]
-        return sum([pow(2, score - 1) for score in winning_cards if score > 0])
+        return sum([2 ** (score - 1) for score in get_winners(self.input) if score > 0])
 
-    # @answer(1234)
+    @answer(5095824)
     def part_2(self) -> int:
-        pass
-
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
+        winners = get_winners(self.input)
+        cards = [1 for w in winners]
+        for i, n in enumerate(winners):
+            self.debug(cards)
+            for j in range(n):
+                cards[i + j + 1] += cards[i]
+        return sum(cards)
