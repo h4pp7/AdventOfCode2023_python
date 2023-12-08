0a89b0b8f956a8bffcbbc02b2f73573c66759f02
Yann Wilhelm <yannwilhelm@yahoo.de>
2023-12-08 23:58:11 +0100

    solution for part 2 of day 8

--- solutions/2023/day_08/solution.py
+++ solutions/2023/day_08/solution.py
@@ -4,31 +4,30 @@
 
 import re
 from itertools import cycle
+from math import lcm
 
 from ...base import StrSplitSolution, answer
 
+
+def count_steps(start, goal, instr, net):
+    node = start
+    steps = 0
+    while not node.endswith(goal):
+        steps += 1
+        node = net[node][next(instr)]
+    return steps
 
 class Solution(StrSplitSolution):
     _year = 2023
     _day = 8
 
-    @answer(14893)
-    def part_1(self) -> int:
+    @answer((14893, 10241191004509))
+    def solve(self) -> tuple[int, int]:
         input = self.input
         instr = cycle([0 if i == "L" else 1 for i in  input[0]])
-        net = {a: (b, c) for a, b , c in [re.findall("[A-Z]+", l) for l in
+        net = {a: (b, c) for a, b , c in [re.findall("[0-9A-Z]+", l) for l in
             input[2:]]}
-        steps = 0
-        node = "AAA"
-        while node != "ZZZ":
-            steps += 1
-            node = net[node][next(instr)]
-        return steps
-
-    # @answer(1234)
-    def part_2(self) -> int:
-        pass
-
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
+        p1 = count_steps("AAA", "ZZZ", instr, net)
+        p2 = lcm(*[count_steps(start, "Z", instr, net) for start in net.keys()
+            if start.endswith("A")])
+        return p1, p2
